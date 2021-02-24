dd8da03a092ef2ff13f7da7197aa11cd37b438a2
neddieseagoon11 <79469572+neddieseagoon11@users.noreply.github.com>
2021-02-24 15:33:54 +0000

    Add files via upload

--- channel.go
+++ channel.go
@@ -1,5 +1,5 @@
-//load data (hardcoded to be coinmarketcap or a test file FTB)
-//at a variety of frequencies and check all coins for breakouts from channels, inform the user of a potentially lucrative event (yeah, right!)
+//load data (hardcoded to be coinmarketcap FTB)
+//at a variety of frequencies (OK, 1 FTB) and check all coins for breakouts from channels, inform the user of a potentially lucrative event (dream on, mate!)
 package main
 
 import (
@@ -15,15 +15,13 @@ import (
 )
 
 type configData struct {
-	Source                     string
-	Testdatafile               string
-	Intervals                  []int
-	Daysofdatainmemory         []int
-	Minnummaximaforchannel     []int
-	Minnumproponoutsidechannel []float64
-	Minnumpointsoutsidechannel []int
-	Maxnumpointsoutsidechannel []int
-	Coinlist                   string
+	Interval      int     //minutes
+	SigIncrease   float64 //increase (proportion) over interval that is considered significant
+	SigNumIncs    int     //Number of consecutive recent significant increases that is considered significant
+	MinRunLen     int     //minimum number of intervals within corridor i.e. how long should it have "wobbled about" before breaking upwards
+	MaxExceptions int     //in the preceding, we are allowed this number of exceptions
+	Corridor      float64 //corridor size as proportion of the price sigNumIncs intervals back
+	Coinlist      string  //file name
 }
 
 type coin struct {
@@ -48,8 +46,8 @@ func main() {
 	}
 	yaml.Unmarshal(data, &config)
 
-	//get list of coins on our exchange
-	coinList := "BTC"
+	//get list of coins on our exchange for use in api call
+	coinList := "BTC" //hardcoding this avoids mucking around with the number of commas
 	prices := make(map[string]*coin)
 	prices["BTC"] = newCoin("BTC")
 	if config.Coinlist != "none" {
@@ -65,7 +63,7 @@ func main() {
 		}
 	}
 
-	tick := config.Intervals[0] //minutes
+	tick := config.Interval //minutes
 
 	//set up request
 	client := &http.Client{}
@@ -74,7 +72,7 @@ func main() {
 	req.Header.Add("Accepts", "application/json")
 	req.Header.Add("X-CMC_PRO_API_KEY", "c0023f87-09da-40a7-815d-5e76ea5750b2")
 
-	//get quotes, forever
+	//get quotes, forever, check for interesting events
 	for {
 		resp, err := client.Do(req)
 		if err != nil {
@@ -94,12 +92,40 @@ func main() {
 			xstuff := quote["USD"]
 			stuff := xstuff.(map[string]interface{})
 			price := stuff["price"].(float64)
-			k := string(xk)
-			prices[k].Prices[prices[k].NumPoints] = price
+			k := string(xk) //key
+			data := prices[k].Prices
+			data[prices[k].NumPoints] = price
 			prices[k].NumPoints++
+			np := prices[k].NumPoints
+			if np >= config.SigNumIncs+config.MinRunLen+1 {
+				interesting := true
+				for counter := np - 1; counter > np-config.SigNumIncs-1; counter-- { //gone up recently?
+					interesting = interesting && (data[counter]/data[counter-1]-1 > config.SigIncrease)
+				}
+				if !interesting {
+					continue //next coin
+				}
+				prevMax := data[np-config.SigNumIncs] //look at a corridor relative to this
+				lenOfRun := 0
+				numExcepts := 0
+				for counter := np - config.SigNumIncs - 1; counter >= 0; counter-- {
+					if data[counter] > (1+config.Corridor)*prevMax || data[counter] < (1-config.Corridor)*prevMax { //check for outside corridor
+						numExcepts++
+						if numExcepts > config.MaxExceptions {
+							interesting = false
+							break
+						}
+					} else {
+						lenOfRun++
+					}
+				}
+				interesting = interesting && (lenOfRun >= config.MinRunLen)
+				if interesting {
+					fmt.Println(k, lenOfRun)
+				}
+			}
 		}
 		resp.Body.Close()
-		fmt.Println(prices["BTC"].Prices[prices["BTC"].NumPoints-1])
 		time.Sleep(time.Duration(tick) * time.Minute)
 	}
 }
